c439423ddf9064087298429a41b4239975bf0745
Marenza Santarin <renziesantarin@gmail.com>
2026-04-28 13:39:11 -0400

    Added sales analysis for Q4 and Q5 (category revenue, store ranking)

--- Santarin_sales_analysis.sql
+++ Santarin_sales_analysis.sql
@@ -60,4 +60,48 @@ ORDER BY Total_Revenue DESC;
 /* LOGIC: Florida is in the South region. This query compares 
 Florida’s revenue with other states in that region. */
 
-/* 4. 
+/* 4. What is the number of transactions per month and average 
+transaction size by product category for the sales territory? */
+
+SELECT DATE_FORMAT(ss.Transaction_Date, '%Y-%m') AS Sales_Month,
+ic.Category,
+COUNT(*) AS Total_Transactions,
+AVG(ss.Sale_Amount) AS Average_Transaction_Size,
+SUM(ss.Sale_Amount) AS Total_Revenue
+FROM store_sales ss
+JOIN store_locations sl ON ss.Store_ID = sl.StoreID
+JOIN Products p ON ss.Prod_Num = p.ProdNum
+JOIN inventory_categories ic ON p.Categoryid = ic.Categoryid
+WHERE sl.State = 'Florida'
+GROUP BY DATE_FORMAT(ss.Transaction_Date, '%Y-%m'), ic.Category
+ORDER BY Sales_Month, Total_Revenue DESC;
+
+/* LOGIC: I need to find the month, category, number of 
+transactions, and the average transaction size. The store_sales 
+table has store ID and sales amount. It also has the product 
+number, which helps link sales to specific products. 
+The products table provides the category information. I grouped 
+the data by month and category. Then, I sorted the results 
+from highest to lowest to rank the categories.*/
+
+/* 5. Can you provide a ranking of in-store sales performance 
+by each store in the sales territory, or a ranking of online 
+sales performance by state within an online sales territory? */
+
+SELECT sl.StoreLocation AS Store_City,
+sl.StoreId AS Store_ID,
+SUM(ss.Sale_Amount) AS Total_Revenue,
+COUNT(*) AS Total_Transactions,
+AVG(ss.Sale_Amount) AS Average_Transaction_Size
+FROM Store_Sales ss
+JOIN Store_Locations sl ON ss.Store_ID = sl.StoreId
+WHERE sl.State = 'Florida'
+GROUP BY sl.StoreLocation, sl.StoreId
+ORDER BY total_revenue DESC;
+
+/* LOGIC: Since Lana manages Florida, I filtered it again to only
+include Florida stores. I started with the store_sales table
+because it has the transaction data, like the value of each sale
+and the store responsible for it. I summed the revenue to measure
+performance. I then grouped them by store and sorted by total
+revenue from highest to lowest to rank the store performance. */
